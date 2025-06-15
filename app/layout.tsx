846deb9723549746fb852fae16539f5d9ec450ae
Sebastian Perez <sebperez@gmail.com>
2025-06-15 23:13:09 +0200

    fix: title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Llibreria Pàgina 128",
+  description: "Llibreria Pàgina 128, pronta per a la seva inauguració",
+  generator: "tausistemas.cl",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
